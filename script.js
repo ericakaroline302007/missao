817146289c984cccf674c88a20054ddafde83579
ericakaroline302007 <111869443+ericakaroline302007@users.noreply.github.com>
2024-06-06 10:34:11 -0300

    ki

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const caixaPrincipal = document.querySelectorAll(.caixaPrincipal)
-const caixaPerguntas = document.querySelectorAll(.caixaPrincipal)
-const caixaAlternativa = document.querySelectorAll(.caixaPrincipal)
-const caixaResultado = document.querySelectorAll(.caixaPrincipal)
-const caixaResultado = document.querySelectorAll(.caixaPrincipal)
+const caixaPrincipal = document.querySelectorAll(".caixaPrincipal");
+const caixaPerguntas = document.querySelectorAll(".caixaPrincipal");
+const caixaAlternativa = document.querySelectorAll(".caixaPrincipal");
+const caixaResultado = document.querySelectorAll(".caixaPrincipal");
+const caixaResultado = document.querySelectorAll(".caixaPrincipal");
 
 const perguntas = {
      //primeiro objeto da lista de perguntas
     enunciado : "pergunta1",
     alternativas : [ "alternativa1" , "alternativa2"]
 },
-    //segundo objeto da lista de perguntas
+    {//segundo objeto da lista de perguntas
    enunciado : "pergunta2",
-   alternativas : [ "alternativa1" , "alternativa2"]
-},
+   alternativas : ["alternativa1" , "alternativa2"]
+    }
+];
 
-]
